87050f8e474e2cd177678f5670fabfd5eb9a7a4e
Akshay Easwaran <akeaswaran@me.com>
2019-11-17 23:33:45 -0800

    commenting out all of the caching code

--- scripts/base.js
+++ scripts/base.js
@@ -52,7 +52,7 @@ module.exports = function(robot) {
             return false
         }
         var lastUpdated = moment(timeString).toNow();
-        if (lastUpdated.includes("seconds") || lastUpdated.includes("minute")) {
+        if (lastUpdated.includes("seconds") || lastUpdated.includes(" minute")) {
             return true
         } else if (lastUpdated.includes("minutes")) {
             var cleaner = lastUpdated.replace("in ","").replace(/ minutes?/i,"");
@@ -62,27 +62,27 @@ module.exports = function(robot) {
         }
     }
 
-    robot.hear(/live/i, (res) => {
-        var gameData = robot.brain.get("most-recent");
-        robot.logger.info(`Loading live again`)
-        if (gameData != null && _isFreshData(gameData.last_updated)) {
-            robot.logger.info(`Referring to existing data, last loaded on ${gameData.last_updated}`)
-            var games = gameData.results;
-            var blocks = generateScoreBlocks(games, true);
-            return sendSlackMessage(res, "", {blocks:JSON.stringify(blocks)}, true);
-        } else {
+    robot.hear(/\!live/i, (res) => {
+        // var gameData = robot.brain.get("most-recent");
+        // robot.logger.info(`Loading live again`)
+        // if (gameData != null && _isFreshData(gameData.last_updated)) {
+        //     robot.logger.info(`Referring to existing data, last loaded on ${gameData.last_updated}`)
+        //     var games = gameData.results;
+        //     var blocks = generateScoreBlocks(games, true);
+        //     return sendSlackMessage(res, "", {blocks:JSON.stringify(blocks)}, true);
+        // } else {
             robot.logger.info(`Loading new data from ESPN...`)
             EspnApi.retrieveFreshCFBGames()
             .then(games => {
                 var blocks = generateScoreBlocks(games, true);
-                robot.brain.set("most-recent",{results:games,last_updated:moment().format()})
+                // robot.brain.set("most-recent",{results:games,last_updated:moment().format()})
                 return sendSlackMessage(res, "", {blocks:JSON.stringify(blocks)}, true);
             })
             .catch(err => {
                 robot.logger.error(`Error while retrieving games from ESPN: ${err}`);
                 return sendSlackMessage(res, `Error while retrieving games from ESPN: ${err}`, null, false);
             })
-        }
+        // }
     });
 
     robot.hear(/\!score\s?(.*)?/i, (res) => {
@@ -92,18 +92,18 @@ module.exports = function(robot) {
             return sendSlackMessage(res, `Did not provide a team name to search for. Please try again with a valid team name.`, null, false);
         } else {
             robot.logger.info(`Loading score again`)
-            var gameData = robot.brain.get("most-recent");
-            if (gameData != null && _isFreshData(gameData.last_updated)) {
-                robot.logger.info(`Referring to existing data, last loaded on ${gameData.last_updated}`)
-                var entries = gameData.results.filter(item => (item.homeTeam.location.toLowerCase().includes(cleanedTeamName) || item.awayTeam.location.toLowerCase().includes(cleanedTeamName)));
-                if (entries.length > 0) {
-                    var blocks = generateScoreBlocks(entries);
-                    return sendSlackMessage(res, "", {blocks:JSON.stringify(blocks)}, true);
-                } else {
-                    robot.logger.error(`Can not find "${cleanedTeamName}" in action at this time.`);
-                    return sendSlackMessage(res, `Can not find "${cleanedTeamName}" in action at this time.`, null, false);
-                }
-            } else {
+            // var gameData = robot.brain.get("most-recent");
+            // if (gameData != null && _isFreshData(gameData.last_updated)) {
+            //     robot.logger.info(`Referring to existing data, last loaded on ${gameData.last_updated}`)
+            //     var entries = gameData.results.filter(item => (item.homeTeam.location.toLowerCase().includes(cleanedTeamName) || item.awayTeam.location.toLowerCase().includes(cleanedTeamName)));
+            //     if (entries.length > 0) {
+            //         var blocks = generateScoreBlocks(entries);
+            //         return sendSlackMessage(res, "", {blocks:JSON.stringify(blocks)}, true);
+            //     } else {
+            //         robot.logger.error(`Can not find "${cleanedTeamName}" in action at this time.`);
+            //         return sendSlackMessage(res, `Can not find "${cleanedTeamName}" in action at this time.`, null, false);
+            //     }
+            // } else {
                 EspnApi.retrieveFreshCFBGames()
                 .then(games => {
                     robot.brain.set("most-recent",{results:games,last_updated:moment().format()})
@@ -120,7 +120,7 @@ module.exports = function(robot) {
                     robot.logger.error(`Error while looking for "${cleanedTeamName}": ${err}`);
                     return sendSlackMessage(res, `Error while looking for "${cleanedTeamName}": ${err}`, null, false);
                 })
-            }
+            // }
         }
     });
 };
